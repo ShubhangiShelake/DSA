78ffcf7765397d3f86b574e6ca1f4ba0a3814cc6
Shubhangi Shelake <136948352+ShubhangiShelake@users.noreply.github.com>
2026-07-08 08:15:32 +0530

    String in DSA

---
+++ ReverseWordsinaSentence.java
@@ -0,0 +1,13 @@
+
+class ReverseWordsinaSentence{
+    public static void main(String[]arg){
+
+        String str="I Love India";
+
+        String arr[]=str.split(" ");
+
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
