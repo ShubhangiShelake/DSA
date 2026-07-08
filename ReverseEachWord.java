374f6e483873b2659e114f65e704b75270e4d2de
Shubhangi Shelake <136948352+ShubhangiShelake@users.noreply.github.com>
2026-07-08 08:16:41 +0530

    String in DSA

---
+++ ReverseEachWord.java
@@ -0,0 +1,14 @@
+class ReverseEachWord {
+    public static void main(String[] arg) {
+        String str = "I Love India";
+
+        String arr[] = str.split(" ");
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = arr[i].length- 1; j >= 0; j--) {
+                System.out.print(arr[i].charAt(j));
+            }
+            System.out.print(" ");
+        }
+    }
+}
